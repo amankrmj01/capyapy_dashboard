de8da24176484bc4d26c84808fe252354c7d8d5b
Aman Kumar Maurya <amankrmj@gmail.com>
2025-08-26 01:38:09 +0530

    update: README.md

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -7,11 +7,11 @@ import '../../presentation/pages/project_details/project_details_page.dart';
 import '../../presentation/bloc/project_details/project_details_bloc.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/dashboard',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const MainPage()),
+    GoRoute(path: '/dashboard', builder: (context, state) => const MainPage()),
     GoRoute(
-      path: '/project/:id',
+      path: '/dashboard/project/:id',
       builder: (context, state) {
         final project = state.extra as Project?;
         return BlocProvider<ProjectDetailsBloc>(
