200281bdb756748b00dc8f9a725ce8a216d5d4e2
Aman Kumar Maurya <amankrmj@gmail.com>
2025-08-26 23:11:43 +0530

    add: stats for the endpoints

---
+++ lib/presentation/pages/project_details/widgets/widgets.dart
@@ -0,0 +1,8 @@
+export 'data_model_editor_dialog.dart';
+export 'data_models_section.dart';
+export 'endpoints_section.dart';
+export 'endpoint_stats.dart';
+export 'project_details_sidebar.dart';
+export 'project_overview_section.dart';
+export 'project_settings_section.dart';
+export 'endpoint_editor_dialog.dart';
