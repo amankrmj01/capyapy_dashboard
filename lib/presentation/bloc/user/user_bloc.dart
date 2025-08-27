6960ea8d71b88f40feeb7841049f54aca4ac5894
Aman Kumar Maurya <amankrmj@gmail.com>
2025-08-28 01:30:37 +0530

    update: user

--- lib/presentation/bloc/user/user_bloc.dart
+++ lib/presentation/bloc/user/user_bloc.dart
@@ -11,64 +11,80 @@ class UserBloc extends Bloc<UserEvent, UserState> {
   final UserRepository userRepository;
 
   UserBloc({required this.userRepository}) : super(UserInitial()) {
-    on<LoadUser>((event, emit) async {
-      emit(UserLoading());
-      try {
-        final user = await userRepository.getUser(event.userId);
-        emit(UserLoaded(user));
-      } catch (e) {
-        emit(UserError(e.toString()));
-      }
-    });
+    on<LoadUser>(_onLoadUser);
+    on<UpdateUser>(_onUpdateUser);
+    on<LogoutUser>(_onLogoutUser);
+    on<AddProjectId>(_onAddProjectId);
+    on<RemoveProjectId>(_onRemoveProjectId);
+    on<GetProjectIds>(_onGetProjectIds);
+  }
 
-    on<UpdateUser>((event, emit) async {
-      emit(UserLoading());
-      try {
-        final user = await userRepository.updateUser(event.user);
-        emit(UserLoaded(user));
-      } catch (e) {
-        emit(UserError(e.toString()));
-      }
-    });
+  Future<void> _onLoadUser(LoadUser event, Emitter<UserState> emit) async {
+    emit(UserLoading());
+    try {
+      final user = await userRepository.getUser(event.userId);
+      emit(UserLoaded(user));
+    } catch (e) {
+      emit(UserError(e.toString()));
+    }
+  }
 
-    on<LogoutUser>((event, emit) async {
-      emit(UserLoading());
-      try {
-        await userRepository.logoutUser();
-        emit(UserInitial());
-      } catch (e) {
-        emit(UserError(e.toString()));
-      }
-    });
+  Future<void> _onUpdateUser(UpdateUser event, Emitter<UserState> emit) async {
+    emit(UserLoading());
+    try {
+      final user = await userRepository.updateUser(event.user);
+      emit(UserLoaded(user));
+    } catch (e) {
+      emit(UserError(e.toString()));
+    }
+  }
 
-    on<AddProjectId>((event, emit) async {
-      emit(UserLoading());
-      try {
-        await userRepository.addProjectId(event.userId, event.projectId);
-        emit(ProjectIdAdded(event.projectId));
-      } catch (e) {
-        emit(ProjectIdError(e.toString()));
-      }
-    });
+  Future<void> _onLogoutUser(LogoutUser event, Emitter<UserState> emit) async {
+    emit(UserLoading());
+    try {
+      await userRepository.logoutUser();
+      emit(UserInitial());
+    } catch (e) {
+      emit(UserError(e.toString()));
+    }
+  }
 
-    on<RemoveProjectId>((event, emit) async {
-      emit(UserLoading());
-      try {
-        await userRepository.removeProjectId(event.userId, event.projectId);
-        emit(ProjectIdRemoved(event.projectId));
-      } catch (e) {
-        emit(ProjectIdError(e.toString()));
-      }
-    });
+  Future<void> _onAddProjectId(
+    AddProjectId event,
+    Emitter<UserState> emit,
+  ) async {
+    emit(UserLoading());
+    try {
+      await userRepository.addProjectId(event.userId, event.projectId);
+      emit(ProjectIdAdded(event.projectId));
+    } catch (e) {
+      emit(ProjectIdError(e.toString()));
+    }
+  }
 
-    on<GetProjectIds>((event, emit) async {
-      emit(UserLoading());
-      try {
-        final ids = await userRepository.getProjectIds(event.userId);
-        emit(ProjectIdsLoaded(ids));
-      } catch (e) {
-        emit(ProjectIdError(e.toString()));
-      }
-    });
+  Future<void> _onRemoveProjectId(
+    RemoveProjectId event,
+    Emitter<UserState> emit,
+  ) async {
+    emit(UserLoading());
+    try {
+      await userRepository.removeProjectId(event.userId, event.projectId);
+      emit(ProjectIdRemoved(event.projectId));
+    } catch (e) {
+      emit(ProjectIdError(e.toString()));
+    }
+  }
+
+  Future<void> _onGetProjectIds(
+    GetProjectIds event,
+    Emitter<UserState> emit,
+  ) async {
+    emit(UserLoading());
+    try {
+      final ids = await userRepository.getProjectIds(event.userId);
+      emit(ProjectIdsLoaded(ids));
+    } catch (e) {
+      emit(ProjectIdError(e.toString()));
+    }
   }
 }
